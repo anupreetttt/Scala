48fa44c7f5d9f7a9f575939b94b5e95feac7f1c7
Anupreet Paulkar <apaulk2@uic.edu>
2022-08-24 07:58:12 -0500

    Functions in scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,21 @@
 object Main {
+
+  object Math {
+    // int addNum(int m, int n) { return }
+    def addNum(m: Int, n: Int): Int = {
+      return m + n
+    }
+
+    def subNum(m: Int, n: Int): Int = {
+      m - n
+    }
+
+    def multiplyNum(m: Int, n: Int): Int = m * n
+
+    def divideNum(m: Int, n: Int) = m / n
+
+  }
+
   def main(args: Array[String]): Unit = {
       val myName = "Anupreet"
       val myAge = 21;
@@ -16,7 +33,7 @@ object Main {
 
 
     var x = 0
-    while(x <= 100) {
+    while(x <= 50) {
       println("Value x = " + x)
       x += 10;
     }
@@ -39,5 +56,34 @@ object Main {
 
     val square = for {i <- 1 to 5} yield { i * i}
     println(square)
+
+    // Match as statement
+
+    val num = 50
+
+    num match {
+      case 10 => println(num);
+      case 20 => println(num);
+      case 30 => println(num);
+      case 40 => println(num);
+      case 50 => println(num);
+
+      case _ => println("Invalid")
+    }
+     // Match as expression
+    val number = num match {
+      case 10 => println(num);
+      case 20 => println(num);
+      case 30 => println(num);
+      case 40 => println(num);
+      case 50 => println(num);
+
+      case _ => println("Invalid")
+    }
+
+    println(Math.addNum(3, 4))
+    println(Math.subNum(3, 4))
+    println(Math.multiplyNum(3, 4))
+    println(Math.divideNum(3, 4))
   }
 }
