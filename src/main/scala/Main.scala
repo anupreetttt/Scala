35ff61957e44b5341aaf1c898dd8c66ba66c605e
Anupreet Paulkar <apaulk2@uic.edu>
2022-08-24 06:23:14 -0500

    String interpolation

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,11 @@
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello Everyone!")
+      val myName = "Anupreet"
+      val myAge = 20.5;
+    println("My name is " + myName + " and my age is " + myAge + ".")
+    println(s"$myName is $myAge years old.") //s interpolation
+    println(f"$myName%s is $myAge%f years old.") // type safe
+    println(raw"Hello \nworld") // prints literal code
+
   }
 }
